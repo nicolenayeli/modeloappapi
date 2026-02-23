bc264575a46b69b266605f64d03fa161f2a99b84
nikizinha <nicolenayti@gmail.com>
2026-02-23 11:51:08 -0300

    carregar, e search

--- src/app/pages/produto/produto-list/produto-list.page.ts
+++ src/app/pages/produto/produto-list/produto-list.page.ts
@@ -11,6 +11,9 @@ import { Vendas } from 'src/app/services/vendas';
 export class ProdutoListPage implements OnInit {
 
   produtos:any[]=[];
+  start: number = 0; // controla o ínicio da busca
+  limit: number = 20; //quantidade de item por página (você decide)
+  palavra: any;  // any é qualquer valo, palavra é qualquer palavra
 
   constructor(private api: Vendas, private router:Router) { }
 
@@ -18,13 +21,44 @@ export class ProdutoListPage implements OnInit {
     this.listar();
     
   }
-  listar(){
-    this.api.operacao({requisicao:'produto-listar'})
+  listar(event?:any, atualizar: boolean=false){
+    // se for um refresh, a gente (nois) reseta o contador e a lista 
+    if(atualizar){
+      this.start = 0;
+      this.produtos=[];
+    }
+// garos e gatas: aqui a gente a quantidade de linhas que queremos exibir e incrementar
+    this.api.operacao({
+      requisicao:'produto-listar',
+      limit:this.limit,
+      start:this.limit })
     .subscribe((retornoDaApi:any)=>{
       if(retornoDaApi.success){
-        this.produtos = retornoDaApi.data;
+        // aqui a gente acrescenta os novos itens aos existentes
+        this.produtos = [retornoDaApi, ...retornoDaApi.data];
+
+        // por fim incrmenta o start para o próximo carrgamento
+        this.start += this.limit // start inicia com zero (0) e aqui nessa linha passa a valer 20, 40, 60 por diante 
       }
+      // Finaliza a animação do componenteque disparou o evento
+         event.target.complete();
+
+      //desativar o infinite scroll se não houver mais dados
+      if(retornoDaApi.data.length < this.limit && event?.target?.disabled!==undefined){ //&& = e, ? = nulo
+        event.target.disabled = true;
+        // opcional: colocar mensagem...toast
+      } 
     });
+  }
+  // puxar para atualizar (Reseta a lista)
+  //metodo
+  atualizar(event:any){
+    this.listar(event, true);
+  }
+
+  //Scroll infinito (Carrega mais...)
+  carregaMais(event:any){
+    this.listar(event, false);
   }
 
 
@@ -34,6 +68,13 @@ export class ProdutoListPage implements OnInit {
   }
   abrirImagem(id:number){
     this.router.navigate(['/produto-imagem', id]);
+  }
+
+
+  //Função disparada pelo SearchBar
+  buscar(event:any){
+    this.palavra = event.target.value.toLowerCase();
+    this.listar(null,true);
   }
 
 
